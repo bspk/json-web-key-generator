47b2fe08ab6bb388befb718de5897c0ae7f48edb
Leonard Brünings <leonard.bruenings@1und1.de>
2016-07-25 19:37:36 +0200

    Cleanup Whitespace and Add Editorconfig

--- src/main/java/org/mitre/jose/jwk/Launcher.java
+++ src/main/java/org/mitre/jose/jwk/Launcher.java
@@ -31,137 +31,137 @@ import com.nimbusds.jose.jwk.KeyUse;
 
 /**
  * Small Helper App to generate Json Web Keys
- *
  */
 public class Launcher {
 
-	private static Options options;
+    private static Options options;
 
     public static void main(String[] args) {
 
-    	options = new Options();
+        options = new Options();
 
-    	options.addOption("t", true, "Key Type, one of: " + KeyType.RSA.getValue() + ", " + KeyType.OCT.getValue() + ", " + KeyType.EC.getValue());
-    	options.addOption("s", true, "Key Size in bits, required for RSA and oct key types. Must be an integer divisible by 8");
-    	options.addOption("u", true, "Usage, one of: enc, sig (optional)");
-    	options.addOption("a", true, "Algorithm (optional)");
-    	options.addOption("i", true, "Key ID (optional), one will be generated if not defined");
+        options.addOption("t", true, "Key Type, one of: " + KeyType.RSA.getValue() + ", " + KeyType.OCT.getValue() + ", " +
+                KeyType.EC.getValue());
+        options.addOption("s", true, "Key Size in bits, required for RSA and oct key types. Must be an integer divisible by 8");
+        options.addOption("u", true, "Usage, one of: enc, sig (optional)");
+        options.addOption("a", true, "Algorithm (optional)");
+        options.addOption("i", true, "Key ID (optional), one will be generated if not defined");
         options.addOption("I", false, "Don't generate a Key ID if none defined");
-    	options.addOption("p", false, "Display public key separately");
-    	options.addOption("c", true, "Key Curve, required for EC key type. Must be one of " + Curve.P_256 + ", " + Curve.P_384 + ", " + Curve.P_521);
-    	options.addOption("S", false, "Wrap the generated key in a KeySet");
-		options.addOption("o", true, "Write output to file (will append to existing KeySet if -S is used), No Display of Key Material");
+        options.addOption("p", false, "Display public key separately");
+        options.addOption("c", true, "Key Curve, required for EC key type. Must be one of " + Curve.P_256 + ", " + Curve.P_384
+				+ ", " + Curve.P_521);
+        options.addOption("S", false, "Wrap the generated key in a KeySet");
+        options.addOption("o", true, "Write output to file (will append to existing KeySet if -S is used), No Display of Key "
+				+ "Material");
 
-    	CommandLineParser parser = new PosixParser();
-    	try {
-	        CommandLine cmd = parser.parse(options, args);
+        CommandLineParser parser = new PosixParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
 
-	        String kty = cmd.getOptionValue("t");
-	        String size = cmd.getOptionValue("s");
-	        String use = cmd.getOptionValue("u");
-	        String alg = cmd.getOptionValue("a");
-	        String kid = cmd.getOptionValue("i");
-	        String crv = cmd.getOptionValue("c");
-	        boolean keySet = cmd.hasOption("S");
-	        boolean pubKey = cmd.hasOption("p");
+            String kty = cmd.getOptionValue("t");
+            String size = cmd.getOptionValue("s");
+            String use = cmd.getOptionValue("u");
+            String alg = cmd.getOptionValue("a");
+            String kid = cmd.getOptionValue("i");
+            String crv = cmd.getOptionValue("c");
+            boolean keySet = cmd.hasOption("S");
+            boolean pubKey = cmd.hasOption("p");
             boolean doNotGenerateKid = cmd.hasOption("I");
-			String outFile = cmd.getOptionValue("o");
+            String outFile = cmd.getOptionValue("o");
 
-	        // check for required fields
-	        if (kty == null) {
-	        	printUsageAndExit("Key type must be supplied.");
-	        }
+            // check for required fields
+            if (kty == null) {
+                printUsageAndExit("Key type must be supplied.");
+            }
 
-	        // parse out the important bits
+            // parse out the important bits
 
-        	KeyType keyType = KeyType.parse(kty);
+            KeyType keyType = KeyType.parse(kty);
 
-        	KeyUse keyUse = null;
-        	if (use != null) {
-        		if (use.equals("sig")) {
-	    			keyUse = KeyUse.SIGNATURE;
-	    		} else if (use.equals("enc")) {
-	    			keyUse = KeyUse.ENCRYPTION;
-	    		} else {
-	    			printUsageAndExit("Invalid key usage, must be 'sig' or 'enc', got " + use);
-	    		}
-        	}
+            KeyUse keyUse = null;
+            if (use != null) {
+                if (use.equals("sig")) {
+                    keyUse = KeyUse.SIGNATURE;
+                } else if (use.equals("enc")) {
+                    keyUse = KeyUse.ENCRYPTION;
+                } else {
+                    printUsageAndExit("Invalid key usage, must be 'sig' or 'enc', got " + use);
+                }
+            }
 
             if (Strings.isNullOrEmpty(kid)) {
                 kid = doNotGenerateKid ? null : generateKid(keyUse);
             }
 
-        	Algorithm keyAlg = null;
-        	if (!Strings.isNullOrEmpty(alg)) {
-        		keyAlg = JWSAlgorithm.parse(alg);
-        	}
+            Algorithm keyAlg = null;
+            if (!Strings.isNullOrEmpty(alg)) {
+                keyAlg = JWSAlgorithm.parse(alg);
+            }
 
-        	JWK jwk = null;
+            JWK jwk = null;
 
-        	if (keyType.equals(KeyType.RSA)) {
-    	        // surrounding try/catch catches numberformatexception from this
-        		if (Strings.isNullOrEmpty(size)) {
-        			printUsageAndExit("Key size (in bits) is required for key type " + keyType);
-        		}
+            if (keyType.equals(KeyType.RSA)) {
+                // surrounding try/catch catches numberformatexception from this
+                if (Strings.isNullOrEmpty(size)) {
+                    printUsageAndExit("Key size (in bits) is required for key type " + keyType);
+                }
 
-    	        Integer keySize = Integer.decode(size);
-        		if (keySize % 8 != 0) {
-        			printUsageAndExit("Key size (in bits) must be divisible by 8, got " + keySize);
-        		}
+                Integer keySize = Integer.decode(size);
+                if (keySize % 8 != 0) {
+                    printUsageAndExit("Key size (in bits) must be divisible by 8, got " + keySize);
+                }
 
-        		jwk = RSAKeyMaker.make(keySize, keyUse, keyAlg, kid);
-        	} else if (keyType.equals(KeyType.OCT)) {
-    	        // surrounding try/catch catches numberformatexception from this
-        		if (Strings.isNullOrEmpty(size)) {
-        			printUsageAndExit("Key size (in bits) is required for key type " + keyType);
-        		}
-    	        Integer keySize = Integer.decode(size);
-        		if (keySize % 8 != 0) {
-        			printUsageAndExit("Key size (in bits) must be divisible by 8, got " + keySize);
-        		}
+                jwk = RSAKeyMaker.make(keySize, keyUse, keyAlg, kid);
+            } else if (keyType.equals(KeyType.OCT)) {
+                // surrounding try/catch catches numberformatexception from this
+                if (Strings.isNullOrEmpty(size)) {
+                    printUsageAndExit("Key size (in bits) is required for key type " + keyType);
+                }
+                Integer keySize = Integer.decode(size);
+                if (keySize % 8 != 0) {
+                    printUsageAndExit("Key size (in bits) must be divisible by 8, got " + keySize);
+                }
 
-        		jwk = OctetSequenceKeyMaker.make(keySize, keyUse, keyAlg, kid);
-        	} else if (keyType.equals(KeyType.EC)) {
-        		if (Strings.isNullOrEmpty(crv)) {
-					printUsageAndExit("Curve is required for key type " + keyType);
-				}
-				Curve keyCurve = Curve.parse(crv);
-				jwk = ECKeyMaker.make(keyCurve, keyUse, keyAlg, kid);
-        	} else {
-        		printUsageAndExit("Unknown key type: " + keyType);
-        	}
-
+                jwk = OctetSequenceKeyMaker.make(keySize, keyUse, keyAlg, kid);
+            } else if (keyType.equals(KeyType.EC)) {
+                if (Strings.isNullOrEmpty(crv)) {
+                    printUsageAndExit("Curve is required for key type " + keyType);
+                }
+                Curve keyCurve = Curve.parse(crv);
+                jwk = ECKeyMaker.make(keyCurve, keyUse, keyAlg, kid);
+            } else {
+                printUsageAndExit("Unknown key type: " + keyType);
+            }
 
             // round trip it through GSON to get a prettyprinter
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             if (outFile == null) {
 
-				System.out.println("Full key:");
+                System.out.println("Full key:");
 
+                printKey(keySet, jwk, gson);
 
-				printKey(keySet, jwk, gson);
+                if (pubKey) {
+                    System.out.println(); // spacer
 
-				if (pubKey) {
-					System.out.println(); // spacer
+                    // also print public key, if possible
+                    JWK pub = jwk.toPublicJWK();
 
-					// also print public key, if possible
-					JWK pub = jwk.toPublicJWK();
-
-					if (pub != null) {
-						System.out.println("Public key:");
-						printKey(keySet, pub, gson);
-					} else {
-						System.out.println("No public key.");
-					}
-				}
-			} else {
+                    if (pub != null) {
+                        System.out.println("Public key:");
+                        printKey(keySet, pub, gson);
+                    } else {
+                        System.out.println("No public key.");
+                    }
+                }
+            } else {
                 writeKeyToFile(keySet, outFile, jwk, gson);
             }
 
-    	} catch (NumberFormatException e) {
-    		printUsageAndExit("Invalid key size: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            printUsageAndExit("Invalid key size: " + e.getMessage());
         } catch (ParseException e) {
-        	printUsageAndExit("Failed to parse arguments: " + e.getMessage());
+            printUsageAndExit("Failed to parse arguments: " + e.getMessage());
         } catch (java.text.ParseException e) {
             printUsageAndExit("Could not parse existing KeySet: " + e.getMessage());
         } catch (IOException e) {
@@ -197,7 +197,7 @@ public class Launcher {
     }
 
     private static void printKey(boolean keySet, JWK jwk, Gson gson) {
-		if (keySet) {
+        if (keySet) {
             JWKSet jwkSet = new JWKSet(jwk);
             JsonElement json = new JsonParser().parse(jwkSet.toJSONObject(false).toJSONString());
             System.out.println(gson.toJson(json));
@@ -205,18 +205,18 @@ public class Launcher {
             JsonElement json = new JsonParser().parse(jwk.toJSONString());
             System.out.println(gson.toJson(json));
         }
-	}
+    }
 
-	// print out a usage message and quit
+    // print out a usage message and quit
     private static void printUsageAndExit(String message) {
-    	if (message != null) {
-    		System.err.println(message);
-    	}
+        if (message != null) {
+            System.err.println(message);
+        }
 
-    	HelpFormatter formatter = new HelpFormatter();
-    	formatter.printHelp( "java -jar json-web-key-generator.jar -t <keyType> [options]", options );
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar json-web-key-generator.jar -t <keyType> [options]", options);
 
-    	// kill the program
-    	System.exit(1);
+        // kill the program
+        System.exit(1);
     }
 }
--- src/main/java/org/mitre/jose/jwk/RSAKeyMaker.java
+++ src/main/java/org/mitre/jose/jwk/RSAKeyMaker.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.mitre.jose.jwk;
 
@@ -15,39 +15,38 @@ import com.nimbusds.jose.jwk.RSAKey;
 
 /**
  * @author jricher
- *
  */
 public class RSAKeyMaker {
 
-	/**
-	 * @param keySize
-	 * @param keyUse
-	 * @param keyAlg
-	 * @param kid
-	 * @return
-	 */
+    /**
+     * @param keySize
+     * @param keyUse
+     * @param keyAlg
+     * @param kid
+     * @return
+     */
     public static RSAKey make(Integer keySize, KeyUse keyUse, Algorithm keyAlg, String kid) {
-    	
-    	try {
-	        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-	        generator.initialize(keySize);
-	        KeyPair kp = generator.generateKeyPair();
-	        
-	        RSAPublicKey pub = (RSAPublicKey) kp.getPublic();
-	        RSAPrivateKey priv = (RSAPrivateKey) kp.getPrivate();
 
-			RSAKey rsaKey = new RSAKey.Builder(pub)
-						.privateKey(priv)
-						.keyUse(keyUse)
-						.algorithm(keyAlg)
-						.keyID(kid)
-						.build();
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(keySize);
+            KeyPair kp = generator.generateKeyPair();
 
-			return rsaKey;
+            RSAPublicKey pub = (RSAPublicKey) kp.getPublic();
+            RSAPrivateKey priv = (RSAPrivateKey) kp.getPrivate();
+
+            RSAKey rsaKey = new RSAKey.Builder(pub)
+                    .privateKey(priv)
+                    .keyUse(keyUse)
+                    .algorithm(keyAlg)
+                    .keyID(kid)
+                    .build();
+
+            return rsaKey;
         } catch (NoSuchAlgorithmException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	        return null;
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
         }
     }
 }
